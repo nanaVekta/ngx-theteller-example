c06cbd2d3f6996160dc3039c9da2524d1006709d
Asante Julius Kyere <asantejuliuskyere@gmail.com>
2022-04-14 19:02:35 +0000

    final commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ngx-theteller-example';
+
+  apiKey = environment.THE_TELLER_API_KEY; // API key. You will get this in your theteller account
+  transactionId = Math.floor(100000000000 + Math.random() * 900000000000).toString(); // A unique 12 digit transaction id
+  amount = 1; // amount to charge
+  currency = 'GHS'; // currency to charge in. If not defined, default is GHS
+  customerEmail = 'test@email.com'; // customer's email
+  returnUrl = 'https://juliusasante.com'; // A url to redirect to after a successful payment. It can be a page showing a successful transaction message to the user
+  paymentMethod = 'both'; //Payment method, options include card, momo and both. If not specified, default is both
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  THE_TELLER_API_KEY: "Your API key"
 };
 
 /*
